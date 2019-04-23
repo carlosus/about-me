e075bfd57309b428e97ac589ed119870686d284e
Carlos Us Santiago <cdus12@gmail.com>
2019-04-23 16:11:10 -0700

    created variables and assigned via getElementById

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const submitButton = document.getElementById('submit-button');
+const heartInput = document.getElementById('heart-answer');
+const hometownInput = document.getElementById('hometown-answer');
+const cityInput = document.getElementById('city-answer');
+const dislikeInput = document.getElementById('dislike-answer');
+const trickInput = document.getElementById('trick-answer');
+const quizResult = document.getElementById('quiz-result');
