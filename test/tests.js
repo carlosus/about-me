9fa280c7bea414d4d7ed2cfbf25e4adf91e6320f
Carlos Us Santiago <cdus12@gmail.com>
2019-04-23 17:00:54 -0700

    wrote tests in QUnit

---
+++ test/tests.js
@@ -0,0 +1,41 @@
+const test = QUnit.test;
+function scoreAnswers(heartAnswer, hometownAnswer, cityAnswer, dislikeAnswer, trickAnswer){
+    let score = 0;
+    return score;
+}
+
+// test('all correct answers is 5', function(assert) {
+//     //Arrange
+//     // Set up your parameters and expectations
+//     const heartAnswer = 'burgers';
+//     const hometownAnswer = 'phoenix';
+//     const cityAnswer = 'los angeles';
+//     const dislikeAnswer = 'crocs';
+//     const trickAnswer = 'yes';
+//     const expected = 5;
+
+//     //Act 
+//     // Call the function you're testing and set the result to a const
+//     const score = scoreAnswers(heartAnswer, hometownAnswer, cityAnswer, dislikeAnswer, trickAnswer);
+
+//     //Assert
+//     assert.equal(score, expected);
+// });
+
+test('all correct answers is 0', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const heartAnswer = 'burgers';
+    const hometownAnswer = 'phoenix';
+    const cityAnswer = 'los angeles';
+    const dislikeAnswer = 'crocs';
+    const trickAnswer = 'yes';
+    const expected = 0;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const score = scoreAnswers(heartAnswer, hometownAnswer, cityAnswer, dislikeAnswer, trickAnswer);
+
+    //Assert
+    assert.equal(score, expected);
+});
