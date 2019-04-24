26f50912f487e2d3227961eb40cc8c510b2583d2
Carlos Us Santiago <cdus12@gmail.com>
2019-04-23 17:52:08 -0700

    final changes

--- src/index.js
+++ src/index.js
@@ -15,10 +15,25 @@ submitButton.addEventListener('click', () => {
     const dislikeAnswer = dislikeInput.value;
     const trickAnswer = trickInput.value;
 
+    if(heartAnswer.trim() === '' || hometownAnswer.trim() === '' || cityAnswer.trim() === '' || dislikeAnswer.trim() === ' ' || trickAnswer.trim() === ' ') {
+        alert('all answers are required');
+        return;
+    }
+
     const score = scoreAnswers(heartAnswer, hometownAnswer, cityAnswer, dislikeAnswer, trickAnswer);
 
     const message = 'You got ' + score + '/5 correct!';
 
     quizResult.textContent = message;
+
+    if(score < 4) {
+        quizResult.classList.add('bad');
+        quizResult.classList.remove('good');
+    }
+    else {
+        quizResult.classList.add('good');
+        quizResult.classList.remove('bad');
+    }
+
 });
 
