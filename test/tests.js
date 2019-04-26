d554a738d286ffb5af0b1bc189dee974b17bf22d
Carlos Us Santiago <cdus12@gmail.com>
2019-04-26 15:45:21 -0700

    removed dead test code. fixed index js

--- test/tests.js
+++ test/tests.js
@@ -18,21 +18,3 @@ test('all correct answers is 5', function(assert) {
     //Assert
     assert.equal(score, expected);
 });
-
-// test('all correct answers is 0', function(assert) {
-//     //Arrange
-//     // Set up your parameters and expectations
-//     const heartAnswer = 'burgers';
-//     const hometownAnswer = 'phoenix';
-//     const cityAnswer = 'los angeles';
-//     const dislikeAnswer = 'crocs';
-//     const trickAnswer = 'yes';
-//     const expected = 0;
-
-//     //Act 
-//     // Call the function you're testing and set the result to a const
-//     const score = scoreAnswers(heartAnswer, hometownAnswer, cityAnswer, dislikeAnswer, trickAnswer);
-
-//     //Assert
-//     assert.equal(score, expected);
-// });
